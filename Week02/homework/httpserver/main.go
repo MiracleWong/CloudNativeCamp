a272ac01820b64f29b80cb63915de13224bdbf23
MiracleWong <cfwr1991@126.com>
2022-04-24 16:23:23 +0800

    4. 当访问 localhost/healthz 时，应返回 200

--- Week02/homework/httpserver/main.go
+++ Week02/homework/httpserver/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
 func main() {
+	http.HandleFunc("/healthz", healthz)
+
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+
+	io.WriteString(w, strconv.Itoa(http.StatusOK))
 
 }
