7ff0a251be44cf2239cc30dfc0ba20466956fa1c
MiracleWong <cfwr1991@126.com>
2022-04-24 16:24:43 +0800

    1. 接收客户端 request，并将 request 中带的 header 写入 response header

--- Week02/homework/httpserver/main.go
+++ Week02/homework/httpserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -19,5 +20,12 @@ func main() {
 func healthz(w http.ResponseWriter, r *http.Request) {
 
 	io.WriteString(w, strconv.Itoa(http.StatusOK))
+
+	//HTTP方法
+	fmt.Println("Request解析开始……")
+	fmt.Println("Request Header: ", r.Header)
+
+	resp := http.Response{Header: r.Header}
+	fmt.Println("Response Header: ", resp.Header)
 
 }
