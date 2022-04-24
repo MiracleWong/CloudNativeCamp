4f7dc3a45276bbb90d0b83de32d0cb435a824d19
MiracleWong <cfwr1991@126.com>
2022-04-24 15:12:04 +0800

    waitgroup demo

--- Week02/lesson/waitgroup/main.go
+++ Week02/lesson/waitgroup/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	waitByWG()
+}
+
+func waitByWG() {
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
